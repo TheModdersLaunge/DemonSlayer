2018972a0bb30eda0ad29496b1f7ed37ef13d74c
WhosAlbercik <whosalbercik@gmail.com>
2023-06-29 10:03:20 +0200

    Initial Commit

---
+++ src/main/java/whosalbercik/demonslayer/DemonSlayer.java
@@ -0,0 +1,18 @@
+package whosalbercik.demonslayer;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("demonslayer")
+public class DemonSlayer {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DemonSlayer() {
+
+    }
+
+}
